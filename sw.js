1fae356759910bc100e8b78e9847e13117ab7618
KristinaRich <48440527+KristinaRich@users.noreply.github.com>
2019-06-20 14:16:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,69 @@
+var staticCacheVersion = "version-1";
+
+/* KR - Callback install*/
+
+self.addEventListener('install', function(event) {
+  console.log("ServiceWorker installed");
+  event.waitUntil(
+    caches.open(staticCacheVersion)
+      .then(function(cache) {
+        console.log('ServiceWorker caching files');
+        return cache.addAll([
+          '/',
+          '/restaurant.html',
+          '/index.html',
+          '/css/styles.css',
+          '/data/restaurants.json',
+          '/js/dbhelper.js',
+          '/js/main.js',
+          '/js/restaurant_info.js',
+          '/img/1.jpg',
+          '/img/2.jpg',
+          '/img/3.jpg',
+          '/img/4.jpg',
+          '/img/5.jpg',
+          '/img/6.jpg',
+          '/img/7.jpg',
+          '/img/8.jpg',
+          '/img/9.jpg',
+          '/img/10.jpg'
+        ]);
+      }).catch(function(err) {
+          console.log(err);
+        })
+  );
+});
+
+/* KR - create an activate callback and delete old caches*/
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('version-') &&
+            cacheName != staticCacheVersion;
+        }).map(function(cacheName) {
+          console.log("Deleting cached files");
+          return caches.delete(cacheVersion);
+        })
+      );
+    })
+  );
+});
+
+/* KR - Return cached response*/
+self.addEventListener('fetch', function(event) {
+  console.log("ServiceWorker fetching data from", event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  			.catch(function(err) {
+  				console.log("Data caching error: ", err);
+  			})
+  	)
+});
